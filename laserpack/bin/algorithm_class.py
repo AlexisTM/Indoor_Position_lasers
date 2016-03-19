8be796d07cfab060bce014d712e0bfd0ce0854dc
Alexis Paques <alexis.paques@gmail.com>
2016-03-19 14:41:42 +0100

    laser class algorithm

--- laserpack/bin/algorithm_class.py
+++ laserpack/bin/algorithm_class.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-algorithm.py
+algorithm_class.py
 
-This script is the Python implementation of the position algorithm.
+This script is the Python implementation of the position algorithm, 
+using the lasers class which allow to make things easier
 
 It converts 4 or 6 readings into positions and the yaw, filtering 
 values via a Kalman Filter
@@ -34,6 +35,7 @@ from __future__ import division
 import rospy
 import mavros
 import tf
+from lasers import lasersController
 from transformations import *
 from algorithm_functions import *
 from laserpack.msg import distance
@@ -50,9 +52,7 @@ def imu_callback(data):
 def raw_lasers_callback(data):
     global raw
     global imu
-    global positions
-    global orientations
-    global laserNumber
+    global lasers
     global pub_position
     # preCorrectionLasers is TODO
     raw = preCorrectionLasers(data)
@@ -62,81 +62,51 @@ def raw_lasers_callback(data):
     roll, pitch, yaw = euler_from_quaternion(quaternion, axes="sxyz")
     q = quaternion_from_euler(roll, pitch, 0, axes="sxyz")
 
-    laser1x = quaternionRotation(positions[0], q)
-    orientation1x = quaternionRotation(orientations[0], q)
-    laser2x = quaternionRotation(positions[1], q)
-    orientation2x = quaternionRotation(orientations[1], q)
-
-    yawMeasured =  getYawInXL(laser1x, orientation1x, raw.lasers[0], orientation_lengths[0], laser2x, orientation2x, raw.lasers[1], orientation_lengths[1])
+    laser1x, orientation1x, laser2x, orientation1x = lasers.preRotateX(self, q)
+    yawMeasured =  getYawInXL(laser1x, orientation1x, raw.lasers[0], lasers.X1.length, laser2x, orientation2x, raw.lasers[1], lasers.X2.length)
 
     q = quaternion_from_euler(roll, pitch, yawMeasured, axes="sxyz")
 
-    # Rotate ALL vectors
-    local_positions = list()
-    local_orientations = list()
-    local_targets = list()
-    for i in range(laserNumber):
-        local_positions.append(quaternionRotation(positions[i], q))
-        local_orientations.append(quaternionRotation(orientations[i], q))
-        local_targets.append(extrapolate(local_positions[i], local_orientations[i], raw.lasers[i]))
+    lasers.target(q, raw)
 
 
+    # target[i][j]
+    # i = index of the laser extrapolated (1 => 6)
+    # j = direction X, Y or Z
     # TODO Add an output filter (Kalman filter on positions and yaw)
     msg = PoseStamped()
-    msg.pose.position.x = (local_targets[0][0] + local_targets[1][0])/2
+    msg.pose.position.x = (target[0][0] + target[1][0])/2
     if(laserNumber == 4) :
-        msg.pose.position.y = local_targets[2][1]
-        msg.pose.position.z = local_targets[3][2]
+        msg.pose.position.y = target[2][1]
+        msg.pose.position.z = target[3][2]
     else : 
-        msg.pose.position.y = (local_targets[2][1] + local_targets[3][1])/2
-        msg.pose.position.z = (local_targets[3][2] + local_targets[4][2])/2
+        msg.pose.position.y = (target[2][1] + target[3][1])/2
+        msg.pose.position.z = (target[4][2] + target[5][2])/2
     msg.pose.orientation.x = q[0]
     msg.pose.orientation.y = q[1]
     msg.pose.orientation.z = q[2]
     msg.pose.orientation.w = q[3]
-
     pub_position(msg)
 
 
 def preCorrectionLasers():
+    global lasers
     global raw
-    global lasers_offsets
-    global laserNumber
     # TODO Handle data, keep old data if outlier/readerror detected
     # TODO Then, edit raw with correct values
-    # TODO Eventual resize data (/100)
     deoffset = list()
-    for i in range(laserNumber):
+    for i in range(lasers.count):
         # Divide by 100 => centimeters to meters
-        deoffset.append((raw.lasers[i]*offset[i][0] + offset[i][1])/100)
+        deoffset.append((raw.lasers[i]*lasers.list[i].offset[0] + lasers.list[i].offset[1])/100)
     return deoffset
 
 # init should get values of posiion of the lasers
 def init():
     global raw
-    global positions
-    global orientations
-    global laserNumber
-    global orientation_lengths
-    global lasers_offsets
+    global lasers
     rospy.init_node('position_algorithm')
+    lasers = lasersController()
     raw = distance()
-    laserNumber = 4
-    positions = ((-15,-21,-4),       \
-                 (7,20,-4),          \
-                 (-31,-2,-4),        \
-                 (32,-2,-4),         \
-                 (-13,-21,-7.5),     \
-                 (10,20,-7.5))
-    orientations = ((1,0,0), (1,0,0), (0,1,0), (0,1,0), (0,0,1), (0,0,1))
-    orientation_lengths = (length(orientations[0]), length(orientations[1]), length(orientations[2]), length(orientations[3]), length(orientations[4]), length(orientations[5]))
-    lasers_offsets =   ((1,-4.45), \
-                        (1,-1.489), \
-                        (1,0), \
-                        (1,0), \
-                        (1,0), \
-                        (1,0))
-	
 
 # listerners listen to ROS
 def subscribers():
@@ -144,7 +114,7 @@ def subscribers():
     imu_sub         = rospy.Subscriber('mavros/imu/data', Imu, imu_callback)
     state_sub       = rospy.Subscriber('lasers/raw', distance, raw_lasers_callback)
     pub_position    = rospy.Publisher('lasers/pose', PoseStamped, queue_size=3)
-	
+    
 
 def main():
     while not rospy.is_shutdown(): 
---
+++ laserpack/bin/lasers.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+lasers.py
+
+This class represents the lasers real configuration to be used in the 
+algorithm. It allows to avoid to hardcode the positions.
+
+This file is part of ILPS (Indoor Laser Positioning System).
+
+ILPS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ILPS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ILPS.  If not, see <http://www.gnu.org/licenses/>.
+
+Software created by Alexis Paques and Nabil Nehri for the UCL 
+in a Drone-Based Additive Manufacturing of Architectural Structures 
+project financed by the MIT Seed Fund
+
+Copyright (c) Alexis Paques 2016
+"""
+
+from math import sqrt
+from transformations import *
+from algorithm_functions import *
+
+# offset is defined as 
+class laser:
+    def __init__(self, position, orientation, offset):
+        self.position = position
+        self.orientation = orientation
+        self.offset = offset
+        self.length = sqrt(orientation[0]*orientation[0] + orientation[1]*orientation[1] + orientation[2]*orientation[2])
+
+
+class lasersController: 
+    def __init__(self):
+        self.X1 = laser((-15,-21,-4), (1,0,0), (1,-4.45))
+        self.X2 = laser((7,20,-4), (1,0,0), (1,-1.489))
+        self.Y1 = laser((-31,-2,-4), (0,1,0), (1,0))
+        self.Y2 = laser((32,-2,-4), (0,1,0), (1,0))
+        self.Z1 = laser((-13,-21,-7.5), (0,0,1), (1,0))
+        self.Z2 = laser((10,20,-7.5), (0,0,1), (1,0))
+        self.list = (X1, X2, Y1, Y2, Z1, Z2)
+        self.count = 6
+
+    def target(self, q, raw):
+        target = list()
+        for laser in self.list:
+            position = quaternionRotation(self.list[i].position, q)
+            orientation = quaternionRotation(self.list[i].orientation, q)
+            target.append(extrapolate(position[i], orientation[i], raw.lasers[i]))
+        return target
+
+    def preRotateX(self, q):
+        # convert imu to a quaternion tuple
+        laser1 = quaternionRotation(self.X1.position, q)
+        orientation1 = quaternionRotation(self.X1.orientation, q)
+        laser2 = quaternionRotation(self.X2.position, q)
+        orientation2 = quaternionRotation(self.X2.orientation, q)
+        return (laser1, orientation1, laser2, orientation2)
+
+
+    def preRotateY(self, q):
+        # convert imu to a quaternion tuple
+        laser1 = quaternionRotation(self.Y1.position, q)
+        orientation1 = quaternionRotation(self.Y1.orientation, q)
+        laser2 = quaternionRotation(self.Y2.position, q)
+        orientation2 = quaternionRotation(self.Y2.orientation, q)
+        return (laser1, orientation1, laser2, orientation2)
