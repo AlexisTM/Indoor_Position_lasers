c48706a6dd45282d67ed61b1ec0d2c8293bb1164
Alexis Paques <alexis.paques@gmail.com>
2016-08-05 18:16:16 -0400

    Task examples

--- laserpack/bin/test.tasks.py
+++ laserpack/bin/test.tasks.py
@@ -17,21 +17,7 @@ rospy.loginfo("Controller initiated")
 signal(SIGINT, signal_handler)
 
 
-tasks = []
 
-"""
-# Typical way to fill from any source
-for data in dataIn :
-    if data.type == "init" :
-        task = init_UAV("Init UAV")
-    if data.type == "target" :
-        task = target("target1", 10,10,10, 0)
-
-    tasks.append(task)
-"""
-
-rospy.loginfo("Adding tasks")
-task1 = init_UAV("Init UAV")
 
 # target("name", destination, yaw, precision_xy, precision_z, precision yaw)
 # destination = Point(0,0,0)
@@ -40,13 +26,53 @@ task1 = init_UAV("Init UAV")
 # precision_z = 0.05 - default
 # precision_yaw = 1 - default
 
-task2 = target("target1", Point(0,0,1))
-task3 = arm("Arming")
+
+""" Solution 1 : From a lot of data, like a JSON input. Note that we can directly call
+Controller.addTask if we use the tasks once.
+
+# Typical way to fill from any source
+for data in dataIn :
+    if data.type == "init" :
+        task = init_UAV("Init UAV")
+    if data.type == "target" :
+        task = target("target1", 10,10,10, 0)
+    tasks.append(task)
+Controller.addTasks(tasks)
+"""
+
+""" Solution 2 : Create tasks separetly, create an array than add the whole array via Controller.addTasks
+Usefull if we create multiple task array we can use later, like if we want to do a circle,
+create an array circle, then if you want to do it again, just add the array again.
+
+tasks = []
+rospy.loginfo("Adding tasks")
+task1 = init_UAV("Init UAV")
+task2 = arm("Arming")
+task3 = target("target1", Point(0,0,1))
 task4 = disarm("Disarming")
-tasks = [task1, task3, task2, task4]
+tasks = [task1, task2, task3, task4]
 Controller.addTasks(tasks)
 rospy.loginfo("Tasks added")
+"""
 
+
+""" Solution 3 :  Directly add tasks via Controller.addTask
+Preferred if we just want to try some tasks, added manually.
+rospy.loginfo("Adding tasks")
+Controller.addTask(init_UAV("Init UAV"))
+Controller.addTask(target("target1", Point(0,0,1))
+Controller.addTask(arm("Arming")
+Controller.addTask(disarm("Disarming")
+rospy.loginfo("Tasks added")
+"""
+
+
+rospy.loginfo("Adding tasks")
+Controller.addTask(init_UAV("Init UAV"))
+Controller.addTask(target("target1", Point(0,0,1))
+Controller.addTask(arm("Arming")
+Controller.addTask(disarm("Disarming")
+rospy.loginfo("Tasks added")
 
 # for i in range(100):
 while True:
